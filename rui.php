c9bda136cc68b0cb769fe761017303a30dba78d6
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-15 12:04:59 +0100

    Update dois.php, rui.php, and 2 more files...

---
+++ rui.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Teste PHP</title>
+</head>
+<body>
+	<?php echo "<h1>Olá mundo</h1>"; ?>
+	<?php echo "Olá, <br> Vila das Aves."; ?>
+</body>
+</html>
---
+++ tres.php
@@ -0,0 +1,71 @@
+<?php
+	$txt1="Eu sou o";
+	$txt2="e na turma"; 
+	$txt3="sou o numero";
+	$txt4="Vou fazer este modulo com";
+	$txtnome="Joao";
+	$Num1="15";
+	$Num2="20";
+	echo $txt1. " ".$txtnome. " " .$txt2. " " .$txt3. " " .$Num1. ", ".$txt4. " " .$Num2. " Valores.";
+
+//Exercicio a (imprimir a frase com o meu nome, numero e turma)
+	echo "<br>";
+	echo "<br>";
+
+	$txtturma="12I-2";
+	$txtnome="Rui";
+	$Num1="10";
+
+	echo $txt1. " ".$txtnome. " " .$txt2. " " .$txtturma. " " .$txt3. " " .$Num1. ", ".$txt4. " " .$Num2. " Valores.";
+
+//Exercicio b
+	echo "<br>";
+	echo "<br>";
+
+	$txt1="Os testes";
+	$txt2="estão no nosso"; 
+	$txt3="disco de rede";
+	$txt4="em";
+	$txt5="\\Curso\Disciplinas\Modulos\Testes\\";
+
+	echo $txt1. " " .$txt2. " " .$txt3. " ".$txt4. " " .$txt5;
+
+//Exercicio c
+	echo "<br>";
+	echo "<br>";
+
+	$txt1="Em PHP,";
+	$txt2="a declaração de um variavel"; 
+	$txt3="implica a precidência";
+	$txt4="de um caráctes especial:";
+	$txt5="o carater $";
+
+	echo $txt1. " " .$txt2. " " .$txt3. " ".$txt4. " " .$txt5;
+
+//Exercicio d
+	echo "<br>";
+	echo "<br>";
+
+	$txt1="O professor";
+	$txt2="chegou perto"; 
+	$txt3="do aluno e";
+	$txt4="exclamou: \"se não";
+	$txt5="estudares, vais";
+	$txt6="reprovar de ano!\".";
+
+	echo $txt1. " " .$txt2. " " .$txt3. " ".$txt4. " " .$txt5. " " .$txt6;
+
+//Exercicio e
+	echo "<br>";
+	echo "<br>";
+
+	$txt1="O professor";
+	$txt2="chegou perto"; 
+	$txt3="do aluno e";
+	$txt4="exclamou: \"se não";
+	$txt5="estudares, vais";
+	$txt6="reprovar de ano!\".";
+
+	echo $txt1. "<br>" .$txt2. "<br>" .$txt3. "<br>".$txt4. "<br>" .$txt5. "<br>" .$txt6;
+
+?>
